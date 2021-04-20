f13591a93cda015d3dce48f5c80e90e33706a6a6
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-04-20 16:38:16 +0200

    server:controllers: added posts routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const User = require("./models/user");
+const Post = require("./models/post");
 const port = 3000;
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
@@ -33,6 +34,18 @@ app.get("/api/users/", async (req, res) => {
 
 	res.send(users);
 });
+app.post("/api/posts/", async (req, res) => {
+	const newPost = new Post(req.body);
+	await newPost.save();
+
+	res.send("Post created!");
+});
+app.get("/api/posts/", async (req, res) => {
+	const posts = await Post.find({}).populate("author", "fName lName _id");
+
+	res.send(posts);
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
