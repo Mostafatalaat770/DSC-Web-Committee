1ca9f25fec077446e18b2a98524f5f5fd736e1d5
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-04-20 16:54:39 +0200

    server:models: added post field for user

--- server/models/user.js
+++ server/models/user.js
@@ -7,6 +7,7 @@ const schema = new mongoose.Schema({
 	username: "string",
 	password: "string",
 	likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+	posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
 	bio: "string",
 });
 module.exports = mongoose.model("User", schema);
