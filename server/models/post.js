ef33f8c331f852a8583fdd61b4bcf463ad669fd0
Mostafa Talaat <mostafatalaat770@gmail.com>
2021-04-20 16:37:55 +0200

    server:models: added missing post values

--- server/models/post.js
+++ server/models/post.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
 	author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-	content: "string",
-	likes: "number",
-	comments: [{ type: "string" }],
+	content: String,
+	date: { type: Date, default: Date.now() },
+	likes: { type: "number", default: 0 },
+	comments: [{ type: String }],
 });
 module.exports = mongoose.model("Post", schema);
